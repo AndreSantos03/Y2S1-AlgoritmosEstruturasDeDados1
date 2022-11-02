b95c5446c9d422cc9222a8e1dc33fd7f5adb02f1
hmgc2002 <hmgc2016@gmail.com>
2022-11-02 12:08:58 +0000

    Made the header for the sort methods

---
+++ src/sort.h
@@ -0,0 +1,20 @@
+/**
+ * Sort.h : funções para ordenar vetores
+ */
+
+#ifndef _SORT_H_
+#define _SORT_H_
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool ordenaAlfabeticamente(pair<string, string> s1, pair<string, string> s2);
+bool ordenaNumericamente(pair<string, string> s1, pair<string, string> s2);
+bool ordenaCrescenteUc(pair<string, vector<pair<string, int>>> s1, pair<string, vector<pair<string, int>>> s2);
+bool ordenaDecrescenteUc(pair<string, vector<pair<string, int>>> s1, pair<string, vector<pair<string, int>>> s2);
+
+#endif
